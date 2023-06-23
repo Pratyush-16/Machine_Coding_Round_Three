d7c5947a86b642640698dc36626fcb00c42c41c4
Pratyush <76037546+Pratyush-16@users.noreply.github.com>
2023-06-23 21:22:06 +0530

    feat: added search input

--- snack-table/src/Pages/SnacksTable.jsx
+++ snack-table/src/Pages/SnacksTable.jsx
@@ -1,77 +1,86 @@
-import React from 'react'
-import "../Pages/SnacksTable.css"
-
+import React, { useState } from "react";
+import "../Pages/SnacksTable.css";
 
 const snacks = [
-    {
-      id: 1,
-      product_name: "Granola Bar",
-      product_weight: "21g",
-      price: 299,
-      calories: 150,
-      ingredients: ["Oats", "Honey", "Nuts", "Dried Fruits"]
-    },
-    {
-      id: 2,
-      product_name: "Fruit and Nut Mix",
-      product_weight: "73g",
-      price: 749,
-      calories: 353,
-      ingredients: [
-        "Almonds",
-        "Cashews",
-        "Dried Cranberries",
-        "Dried Blueberries"
-      ]
-    },
-    {
-      id: 3,
-      product_name: "Veggie Chips",
-      product_weight: "28g",
-      price: 279,
-      calories: 130,
-      ingredients: ["Sweet Potatoes", "Beets", "Kale", "Sea Salt"]
-    },
-    {
-      id: 4,
-      product_name: "Protein Balls",
-      product_weight: "100g",
-      price: 499,
-      calories: 318,
-      ingredients: ["Dates", "Almond Butter", "Protein Powder", "Chia Seeds"]
-    }
-  ];
+  {
+    id: 1,
+    product_name: "Granola Bar",
+    product_weight: "21g",
+    price: 299,
+    calories: 150,
+    ingredients: ["Oats", "Honey", "Nuts", "Dried Fruits"],
+  },
+  {
+    id: 2,
+    product_name: "Fruit and Nut Mix",
+    product_weight: "73g",
+    price: 749,
+    calories: 353,
+    ingredients: [
+      "Almonds",
+      "Cashews",
+      "Dried Cranberries",
+      "Dried Blueberries",
+    ],
+  },
+  {
+    id: 3,
+    product_name: "Veggie Chips",
+    product_weight: "28g",
+    price: 279,
+    calories: 130,
+    ingredients: ["Sweet Potatoes", "Beets", "Kale", "Sea Salt"],
+  },
+  {
+    id: 4,
+    product_name: "Protein Balls",
+    product_weight: "100g",
+    price: 499,
+    calories: 318,
+    ingredients: ["Dates", "Almond Butter", "Protein Powder", "Chia Seeds"],
+  },
+];
 
 export default function SnacksTable() {
 
+    const [searchInput, setSearchInput] = useState('');
 
+
+    
   return (
     <div>
-        <table>
-<thead>
-  <tr>
-    <th>ID</th>
-    <th>Product Name</th>
-    <th>Product Weight</th>
-    <th>Price</th>
-    <th>Calories</th>
-    <th>Ingredients</th>
-  </tr>
-</thead>
-<tbody>
-  {snacks.map((snack) => (
-    <tr key={snack.id}>
-      <td>{snack.id}</td>
-      <td>{snack.product_name}</td>
-      <td>{snack.product_weight}</td>
-      <td>{snack.price}</td>
-      <td>{snack.calories}</td>
-      <td>{snack.ingredients.join(", ")}</td>
-    </tr>
-  ))}
-</tbody>
-</table>y
+
+    <input
+        type="text"
+        placeholder="Search snacks..."
+        value={searchInput}
+        // onChange={handleSearchInputChange}
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Product Name</th>
+            <th>Product Weight</th>
+            <th>Price</th>
+            <th>Calories</th>
+            <th>Ingredients</th>
+          </tr>
+        </thead>
+        <tbody>
+          {snacks.map((snack) => (
+            <tr key={snack.id}>
+              <td>{snack.id}</td>
+              <td>{snack.product_name}</td>
+              <td>{snack.product_weight}</td>
+              <td>{snack.price}</td>
+              <td>{snack.calories}</td>
+              <td>{snack.ingredients.join(", ")}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
       
     </div>
-  )
+  );
 }
